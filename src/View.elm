c8e230588301779840e2a5640116603643a3065e
raorao <srinivas.rao@stitchfix.com>
2018-07-03 18:54:34 -0700

    title doesn't wrap on mobile

--- src/View.elm
+++ src/View.elm
@@ -1,7 +1,6 @@
 module View exposing (view)
 
 import Html exposing (Html)
-import Html.Attributes
 import Model exposing (Model(..))
 import Update exposing (Msg)
 import Page.Loading
@@ -16,7 +15,7 @@ import Bootstrap.Utilities.Spacing as Spacing
 
 view : Model -> Html Msg
 view model =
-    Grid.containerFluid [ Spacing.mt4, Html.Attributes.style [ ( "padding", "0" ) ] ]
+    Grid.containerFluid [ Spacing.mt4 ]
         [ pageView model
         ]
 
